da7c75cad9e057605317f3c7d2ab075d1ca339c7
Alex Perea <apexmd21@gmail.com>
2013-09-13 14:52:30 -0500

    corregido logout de campo

--- campo/logout.php
+++ campo/logout.php
@@ -1,8 +1,14 @@
 <? 
 session_start();
-session_unregister("usuario");
+/*session_unregister("usuario");
 session_destroy();
-session_unset();
+session_unset();*/
+
+foreach( $_SESSION as $key => $value ){
+
+	unset( $_SESSION[$key] );
+}
+
 $parametros_cookies = session_get_cookie_params();
 setcookie(session_name(),0,1, $parametros_cookies["path"]);
 ?>
